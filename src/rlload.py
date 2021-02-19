1ba409a59d05c2e27757aa11e92d231b07f0e21a
Vi Hồ Phong <hophongmc@gmail.com>
2021-02-19 14:03:30 +0700

    Update rlload.py

--- src/rlload.py
+++ src/rlload.py
@@ -3,17 +3,6 @@ import struct
 import numpy as np
 import re
 
-elements={"h": 1, "he": 2, "li": 3, "be": 4, "b": 5, "c": 6, "n": 7, "o": 8, "f": 9, "ne": 10, "na": 11, "mg": 12, "al": 13, 
-"si": 14, "p": 15, "s": 16, "cl": 17, "ar": 18, "k": 19, "ca": 20, "sc": 21, "ti": 22, "v": 23, "cr": 24, "mn": 25, "fe": 26,
- "co": 27, "ni": 28, "cu": 29, "zn": 30, "ga": 31, "ge": 32, "as": 33, "se": 34, "br": 35, "kr": 36, "rb": 37, "sr": 38, "y": 39,
-  "zr": 40, "nb": 41, "mo": 42, "tc": 43, "ru": 44, "rh": 45, "pd": 46, "ag": 47, "cd": 48, "in": 49, "sn": 50, "sb": 51, "te": 52,
-   "i": 53, "xe": 54, "cs": 55, "ba": 56, "la": 57, "ce": 58, "pr": 59, "nd": 60, "pm": 61, "sm": 62, "eu": 63, "gd": 64, "tb": 65,
-    "dy": 66, "ho": 67, "er": 68, "tm": 69, "yb": 70, "lu": 71, "hf": 72, "ta": 73, "w": 74, "re": 75, "os": 76, "ir": 77, "pt": 78,
-     "au": 79, "hg": 80, "tl": 81, "pb": 82, "bi": 83, "po": 84, "at": 85, "rn": 86, "fr": 87, "ra": 88, "ac": 89, "th": 90, "pa": 91,
-      "u": 92, "np": 93, "pu": 94, "am": 95, "cm": 96, "bk": 97, "cf": 98, "es": 99, "fm": 100, "md": 101, "no": 102, "lr": 103, "rf": 104,
-       "db": 105, "sg": 106, "bh": 107, "hs": 108, "mt": 109, "ds": 110, "rg": 111, "cn": 112, "nh": 113, "fl": 114, "mc": 115, "lv": 116, "ts": 117, "og": 118,
-       "al-":119, "al*": 120}
-
 def getnamebyz(z):
 	"""
 	Get element name by inputing atomic number z
@@ -21,54 +10,6 @@ def getnamebyz(z):
 	Parameter:
 	   z(int): atomic number z
 	"""
-	keys=elements.keys()
-	values=elements.values()
-	return keys[values.index(z)]
-
-def getZ(input):
-	"""
-	Get atomic number z by inputing isotope name
-
-	Parameter:
-		input(str): isotope name (example: zr110)
-	"""
-	if (input==""):
-		return -8888
-	else:
-		sep=re.split('(\d+)',input)
-		if len(sep)==1:
-			if sep[0]=="n":
-				return int(0)
-			elif (sep[0]=="p" or sep[0]=="d" or sep[0]=="t"):
-				return int(1)			
-			else:
-				print "Something wrong! ",input
-		else:
-			return int(elements[sep[0]])
-
-def getA(input):
-	"""
-	Get mass number A by inputing isotope name
-
-	Parameter:
-		input(str): isotope name (example: zr110)
-	"""
-	if (input==""):
-		return -9999
-	else:
-		sep=re.split('(\d+)',input)
-		if len(sep)==1:
-			if sep[0]=="n":
-				return 1
-			elif sep[0]=="p":
-				return 1
-			elif sep[0]=="d":
-				return 2
-			elif sep[0]=="t":
-				return 3
-			else:
-				print "Something wrong! ",input
-		else:
-			return int(sep[1])
+	print z
 
 getnamebyz(10)
